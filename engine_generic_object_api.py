01233a093682b7ae637dbf42922ce30a5e03293c
Guilherme Carvalho Chehab <guilherme.chehab@presidencia.gov.br>
2018-10-14 06:08:54 -0700

    Add GetProperties method to generic object API

--- engine_generic_object_api.py
+++ engine_generic_object_api.py
@@ -14,7 +14,15 @@ class EngineGenericObjectApi:
         except KeyError:
             return response["error"]
 
-    def get_hypercube_data(self, handle, path="/qHyperCubeDef", pages=[]):
+    def get_properties(self, handle):
+        msg = json.dumps({"jsonrpc": "2.0", "id": 0, "handle": handle, "method": "GetProperties", "params": []})
+        response = json.loads(self.engine_socket.send_call(self.engine_socket, msg))
+        try:
+            return response["result"]
+        except KeyError:
+            return response["error"]
+
+     def get_hypercube_data(self, handle, path="/qHyperCubeDef", pages=[]):
         msg = json.dumps({"jsonrpc": "2.0", "id": 0, "handle": handle, "method": "GetHyperCubeData",
                           "params": [path,pages]})
         response = json.loads(self.engine_socket.send_call(self.engine_socket, msg))
