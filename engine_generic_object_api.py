1e62f8160ea508caa1a3ea61cd5d74a7e53aa66d
Guilherme Carvalho Chehab <guilherme.chehab@presidencia.gov.br>
2018-10-14 06:12:26 -0700

    Add GetHyperCubePivotData method to generic object API

--- engine_generic_object_api.py
+++ engine_generic_object_api.py
@@ -22,8 +22,17 @@ class EngineGenericObjectApi:
         except KeyError:
             return response["error"]
 
-     def get_hypercube_data(self, handle, path="/qHyperCubeDef", pages=[]):
+    def get_hypercube_data(self, handle, path="/qHyperCubeDef", pages=[]):
         msg = json.dumps({"jsonrpc": "2.0", "id": 0, "handle": handle, "method": "GetHyperCubeData",
+                          "params": [path,pages]})
+        response = json.loads(self.engine_socket.send_call(self.engine_socket, msg))
+        try:
+            return response["result"]
+        except KeyError:
+            return response["error"]
+
+    def get_hypercube_pivot_data(self, handle, path="/qHyperCubeDef", pages=[]):
+        msg = json.dumps({"jsonrpc": "2.0", "id": 0, "handle": handle, "method": "GetHyperCubePivotData",
                           "params": [path,pages]})
         response = json.loads(self.engine_socket.send_call(self.engine_socket, msg))
         try:
